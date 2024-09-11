e3d79952b8eb63a59c1fc54bca55133aad0f71b3
khonsazulfa <105642292+khonsazulfa@users.noreply.github.com>
2024-09-11 14:24:46 +0100

    Update earth-engine-scripts.js

--- earth-engine-scripts.js
+++ earth-engine-scripts.js
@@ -1,3 +1,4 @@
+// Select area of case study
 var imageCollection = ee.ImageCollection("COPERNICUS/S1_GRD"),
     geometry = 
     /* color: #d63000 */
@@ -13,21 +14,23 @@ var imageCollection = ee.ImageCollection("COPERNICUS/S1_GRD"),
           [114.91198767261514, -3.4790401501785615],
           [114.91198767261514, -3.2747751904408777]]], null, false);
 
-
 Map.centerObject (geometry);
 
-var time_start = '2021', time_end = '2022'
 
+var time_start = '2021', time_end = '2022' // Select which year and month of image data we want to collect
+
+// Reduce speckle noise in an image
 function speckel(img){
   return img.focalMedian(100, 'square', 'meters')
   .copyProperties(img, img.propertyNames())
   }
 
+// Let's proceed the image on 2021 January (during the flood)
 var before = imageCollection
 .filterDate (time_start, time_end)
-.filter(ee.Filter.calendarRange(1, 1, 'month'))
+.filter(ee.Filter.calendarRange(1, 1, 'month')) // 1 means January (first month)
 .filterBounds (geometry)
-.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))
+.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV')) // filtering, processing, and aggregating radar imagery
 .filter(ee.Filter.eq('instrumentMode', 'IW'))
 .map(speckel)
 .select('VV')
@@ -35,11 +38,12 @@ var before = imageCollection
 
 Map.addLayer(before.clip(geometry),[],'before', false);
 
+// Let's proceed the image on 2021 July (after the flood)
 var after = imageCollection
 .filterDate (time_start, time_end)
-.filter(ee.Filter.calendarRange(7, 7, 'month'))
+.filter(ee.Filter.calendarRange(7, 7, 'month')) // 7 means July (7th month)
 .filterBounds (geometry)
-.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))
+.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV')) // filtering, processing, and aggregating radar imagery
 .filter(ee.Filter.eq('instrumentMode', 'IW'))
 .map(speckel)
 .select('VV')
@@ -48,14 +52,15 @@ var after = imageCollection
 Map.addLayer(after.clip(geometry),[],'after', false);
 
 
-var change = before.subtract(after).rename('flood')
+var change = before.subtract(after).rename('flood') // Changes of image between before and after 
 
 Map.addLayer(change.clip(geometry),[],'flood', false)
 
 print(
-  ui.Chart.image.histogram(change, geometry, 100)
+  ui.Chart.image.histogram(change, geometry, 100) // To understand the distribution of flood (in pixel values) within the image.
   )
   
+//Calculate the flood area
 var flood_thr = change.gt(2);
 var flood_mask = flood_thr.updateMask(flood_thr);
 var flood_area = flood_mask.multiply(ee.Image.pixelArea().divide(1e6));
